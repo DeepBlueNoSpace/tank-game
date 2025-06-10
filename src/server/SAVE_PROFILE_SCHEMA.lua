58311ff3e9e7331e795a5078804d332219e9434a
Austin Tindal <49575558+atindal@users.noreply.github.com>
2025-06-10 00:34:40 -0400

    WIP UPGRADES

--- src/server/SAVE_PROFILE_SCHEMA.lua
+++ src/server/SAVE_PROFILE_SCHEMA.lua
@@ -10,11 +10,34 @@ DATASCHEMA.OwnedTanks = {
     ["Sherman"] = true,
 }
 
+DATASCHEMA.TankUpgrades = {
+    -- NOTE!
+    -- For now, equipped has Munition1 and Munition2
+    -- But OWNED has Munitions (shared ownership pool)
+
+    OwnedUpgrades = {},
+    --[[
+        [TankName] = {
+            Engine = {
+                ["UpgradeName"] = true
+            }
+        }
+    ]]
+
+
+    EquippedUpgrades = {}
+     --[[
+        [TankName] = {
+            Engine = 'upgradeName'
+        }
+    ]]
+}
+
 DATASCHEMA.Currency = {}
 for currencyName, data in pairs(CurrencyData) do
     DATASCHEMA.Currency[currencyName] = data.DefaultValue or 0
 end
-
+ 
 DATASCHEMA.LevelData = {
     Level = 1,
     XP = 0,
