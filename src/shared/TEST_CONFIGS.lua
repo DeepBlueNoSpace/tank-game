65743f1044069e14aec4bca0b26421b62c401a2f
Nicolas Markham <nicolasjmarkham@gmail.com>
2025-06-20 09:36:32 +0100

    add test config for visualising the ray

--- src/shared/TEST_CONFIGS.lua
+++ src/shared/TEST_CONFIGS.lua
@@ -1,3 +1,4 @@
+--!strict
 local TEST_CONFIGS = {}
 TEST_CONFIGS.TESTING = true
 
@@ -5,14 +6,14 @@ TEST_CONFIGS.TESTING = true
 TEST_CONFIGS.CLEAN_DATA = true -- data is fresh each time
 
 TEST_CONFIGS.TANK_DMG = false  -- force set damage all tank bullets do
+TEST_CONFIGS.VISUALISE_AIM = false  -- force set damage all tank bullets do
 
 TEST_CONFIGS.SOUNDTRACK_OFF = true
-
 
 local PLACEID = 138314303849362
 local STAGINGID = 0 --112899678167402 SUCK MY BALLS TEST CONFIGS SHOULD BE ON IN STAGING TURN THEM OFF IF U DONT WANT EM
 
-function TEST_CONFIGS.Is(value)	
+function TEST_CONFIGS.Is(value: string): boolean
 	return TEST_CONFIGS.TESTING and TEST_CONFIGS[value]
 end
 
