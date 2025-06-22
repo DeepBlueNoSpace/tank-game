c0c17e3a035542757ab1e1b58f341af9360495a8
Austin Tindal <49575558+atindal@users.noreply.github.com>
2025-06-22 02:57:07 -0400

    post-game scenes

--- src/shared/TEST_CONFIGS.lua
+++ src/shared/TEST_CONFIGS.lua
@@ -5,7 +5,9 @@ TEST_CONFIGS.TESTING = true
 --configurables
 TEST_CONFIGS.CLEAN_DATA = true -- data is fresh each time
 
-TEST_CONFIGS.TANK_DMG = false  -- force set damage all tank bullets do
+TEST_CONFIGS.ROUND_TIME = 60 -- force round time to be X seconds, FALSE if not usig this
+
+TEST_CONFIGS.TANK_DMG = 999  -- force set damage all tank bullets do
 TEST_CONFIGS.VISUALISE_AIM = false  -- force set damage all tank bullets do
 
 TEST_CONFIGS.SOUNDTRACK_OFF = true
